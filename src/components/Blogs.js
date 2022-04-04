9f5a39608a17f14055f73591c5de3ea33e0bd1bc
Shadekul Shovo <shadekuljamalpur.bd21@gmail.com>
2022-04-04 22:51:58 +0600

    Doing Questions

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -15,6 +15,16 @@ const Blogs = () => {
         management frameworks like Redux. It's easy to obtain data from the
         store using libraries like Redux. But let's focus on the Context API.
       </p>
+      <h3>Question2: What is semantic tag?</h3>
+      <p>
+        Semantic Semantic markup (HTML) is HTML that adds meaning to a web page
+        rather than merely displaying it. HTML 5 is semantic. Header, Figure,
+        Caption, Section, Nav. HTML Semantic tags help the browser understand
+        the page's purpose and content. Semantic tags help SEO by making
+        information more flexible. Semantic components help screen readers a
+        lot. Semantic tags give extra information that helps screen readers
+        comprehend text and navigate pages more effectively.
+      </p>
     </div>
   );
 };
