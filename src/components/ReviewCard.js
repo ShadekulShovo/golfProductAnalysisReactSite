6bd40c50bcdbb5a36159cc27c884d7189629bca2
Shadekul Shovo <shadekuljamalpur.bd21@gmail.com>
2022-04-04 22:53:33 +0600

    Almost Dones

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -5,19 +5,6 @@ const ReviewCard = ({ rev }) => {
   const { name, id, picture, review, rating } = rev;
   return (
     <div>
-      {/* <div className="card">
-        <div className="card-info align-items-center p-3">
-          <div className="image">
-            <img src={picture} alt="" />
-          </div>
-          <div>
-            <p>Name: {name}</p>
-            <p>Review: {review}</p>
-            <p>Rating: {rating}</p>
-          </div>
-        </div>
-      </div> */}
-
       <div class="card">
         <img src={picture} class="card-img-top" alt="" />
         <div class="card-body">
